8feb9c8fc5fc309c0b8680a24035f5dba0cec779
mugaboshafidanny <mugabodannyshafi@gmail.com>
2024-05-31 13:00:19 +0200

    setting up

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      <span className='text-blue-400'>hello world</span>
+    </div>
+  );
+}
+
+export default App;
